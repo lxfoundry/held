50806c7accf5345f41778d983325337283d41d0f
levalleux-ludo <levalleux_ludo@hotmail.com>
2026-09-03 19:40:03 +0100

    fix(demo-helper): update prompts for clarity and consistency

--- demo-helper.cmd
+++ demo-helper.cmd
@@ -1,19 +1,22 @@
 @echo off
+
+start cmd /C "npm run buyer"
+
 :round1
 call npm run demo-reset -- --execute
 call npm run mediate 241
 explorer "http://127.0.0.1:3100/?purchase=241"
 
-set /p input=Click """Add a photo""" and Press any key...
+set /p input=Click "Add a photo" and Press Enter...
 call npm run mediate 241
 
-set /p input=Press any key to continue to round 2b...
+set /p input=Press Enter to continue to round 2b...
 :round2b
 call npm run demo-reset -- --round 2b --execute
 call npm run mediate 241
 explorer "http://127.0.0.1:3100/?purchase=241&photo=carton-crushed"
 
-set /p input=Press any key to continue to round 2c...
+set /p input=Press Enter to continue to round 2c...
 :round2c
 call npm run demo-reset -- --round 2c --execute
 call npm run mediate 241
